df19639ac73feb5380fc18444b918879eb2e84d4
lorenzobini <lrnbini@gmail.com>
2021-10-12 15:32:59 +0200

    Function implementation and directory check

--- src/main.py
+++ src/main.py
@@ -4,21 +4,44 @@ import mne_nirs
 from interpolation import interpolate_channels
 
 
-files = pick_files()
-for file in files:
-    recording = load_recording(file)
+def main():
+    files = pick_files()
+    for file in files:
+        recording = load_recording(file)
 
-    # ## NORMALIZING DATA
-    recording = recording.copy().apply_function(fun=normalize,
-                                                picks=recording.info.ch_names,
-                                                channel_wise=True)
+        # ## NORMALIZING DATA
+        recording = recording.copy().apply_function(fun=normalize,
+                                                    picks=recording.info.ch_names,
+                                                    channel_wise=True)
 
-    # ## RETRIEVING LIST OF CHANNELS TO INTERPOLATE
-    chs_to_interpolate = read_channels_to_interpolate()
-    chs_to_interpolate = append_hbohbr(chs_to_interpolate)
+        # ## RETRIEVING LIST OF CHANNELS TO INTERPOLATE
+        chs_to_interpolate = read_channels_to_interpolate()
+        chs_to_interpolate = append_hbohbr(chs_to_interpolate)
 
 
-    # ## INTERPOLATING
-    methods = ["nearest", "linear", "quadratic", "cubic", "bilinear", "bicubic", "quintic"]
-    for method in methods:
-        interp_recording = interpolate_channels(recording.copy(), chs_to_interpolate, method=method)
+        # ## INTERPOLATING
+        methods = ["nearest", "linear", "quadratic", "cubic", "bilinear", "bicubic", "quintic"]
+        for method in methods:
+            interp_recording = interpolate_channels(recording.copy(), chs_to_interpolate, method=method)
+
+
+if __name__ == "__main__":
+    global filepath
+    os.makedirs("data", exist_ok=True)
+    filepath = os.path.join(os.getcwd(), 'data') + os.sep
+
+    if os.path.isfile(filepath + "channels_to_interpolate.txt"):
+        if os.stat("file").st_size == 0:
+            ImportError("The file channels_to_interpolate.txt is empty. Please specify a list of channels to interpolate.")
+    else:
+        f = open(os.path.isfile(filepath + "channels_to_interpolate"), "w+")
+        f.close()
+        ImportError("The file channels_to_interpolate.txt did not exist. An empty file has been created. Please specify a list of channels to interpolate.")
+
+    if os.path.isfile(filepath + "channels_to_exclude.txt"):
+        if os.stat("file").st_size == 0:
+            ImportWarning("The file channels_to_exclude.txt is empty. Please specify eventual channels to ignore to avoid errors.")
+    else:
+        f = open(os.path.isfile(filepath + "channels_to_exclude"), "w+")
+        f.close()
+        ImportError("The file channels_to_exclude.txt did not exist. An empty file has been created. Please specify eventual channels to ignore to avoid errors.")
--- src/utils.py
+++ src/utils.py
@@ -129,3 +129,22 @@ def append_hbohbr(chs, values=False):
             ch_names_hbohbr.append(ch_name + " hbr")
 
     return ch_names_hbohbr
+
+
+def channels_indices(rec: Raw, chs: [str]):
+    """
+    Determines the indices of the given channels within the recording object
+    :param rec:
+    :param chs:
+    :return:
+    """
+    chs_indices = []
+    rec_df = rec.copy().to_data_frame()
+    for ch in chs:
+        if ch not in rec.ch_names:
+            RuntimeWarning(f"Could not determine the index of channel {ch} in recording.")
+        # Determining the patch channels' index
+        ch_index = rec_df.columns.get_loc(ch) - 1
+        chs_indices.append(ch_index)
+
+    return chs_indices
